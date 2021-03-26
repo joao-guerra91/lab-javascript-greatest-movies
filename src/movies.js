e8ce47c9c4b7b915ee995b57a617bea45f8ecc64
Joao Guerra <joao.pedro.guerra@hotmail.com>
2021-03-26 15:57:08 +0000

    Lab greatest movies

--- src/movies.js
+++ src/movies.js
@@ -1,11 +1,41 @@
 // Iteration 1: All directors? - Get the array of all directors.
+const getAllDirectors = (arr) => {
+  const newArr = arr.map((movie) => {
+    return movie.director;
+  });
+  return newArr;
+}
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors. How could you "clean" a bit this array and make it unified (without duplicates)?
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
+const howManyMovies = (arr) => {
+  const newArr = arr.filter((movie) => {
+    return movie.director === "Steven Spielberg" && movie.genre.includes('Drama')
+  })
+
+  return newArr.length;
+}
+//console.log(howManyMovies())
+
+
+
 
 // Iteration 3: All rates average - Get the average of all rates with 2 decimals
+const ratesAverage = (arr) => {
+  const avgArr = arr.reduce((accumulator, currentValue) => {
+    return (accumulator.rate + currentValue.rate)/ arr.length;
+  });
+  // console.log(avgArr)
+  return avgArr;
+};
+
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
+
+// const dramaMoviesRate = (arr) => {
+
+// }
+
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
 
